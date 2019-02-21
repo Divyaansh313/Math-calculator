e02f690df2ea4cf04fa2a0bdad4e8f5b65061065
Divyaansh Jain <divyaansh03anu@gmail.com>
2019-02-21 10:56:42 +0530

    formation of mathematical programs

---
+++ calci.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<math.h> 
+using namespace std;
+class CALC
+{
+	public:
+		int a, b, sum, diff, multiply, divide;
+
+	void sum()
+	{
+		sum = a + b;
+	}
+	void diff()
+	{
+		diff = a - b;
+	}
+	void multiply()
+	{
+		multiply = a * b;
+	}
+	void divide()
+	{
+		divide = a / b;
+	}
+
+	
+
